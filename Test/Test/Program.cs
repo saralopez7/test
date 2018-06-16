cebdaa6970a39983f7da53d1458451f1fe26b639
SorrowHumanity <wpckishere@gmail.com>
2018-06-16 17:02:38 +0200

    асд

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -10,6 +10,8 @@ namespace Test
     {
         private static void Main(string[] args)
         {
+            //асдасдасдадс
+
             Console.WriteLine("Hello World!!");
             Console.ReadKey();
         }
